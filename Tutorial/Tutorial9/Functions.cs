699eabcdade9bbb052bd8bab4ab6254b3417a6a3
yuka1984 <yuka.abn@sigmact.com>
2019-04-07 13:05:31 +0900

    Add Tutorial9

---
+++ Tutorial/Tutorial9/Functions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using DurableTask.Core;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace Tutorial9
+{
+    public class TimerFunctions
+    {
+        [FunctionName(nameof(Purge))]
+        public async Task Purge([TimerTrigger("0 0 20 * * *")]TimerInfo timerInfo, [OrchestrationClient]DurableOrchestrationClient durableClient)
+        {
+            await durableClient.PurgeInstanceHistoryAsync(
+                DateTime.MinValue,
+                DateTime.UtcNow.AddMonths(-1),
+                new[] {OrchestrationStatus.Completed});
+        }
+    }
+}
